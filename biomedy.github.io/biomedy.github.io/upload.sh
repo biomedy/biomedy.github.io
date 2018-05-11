e11ef8fad0158a2fcbc0c42f2a8896619b0acc9c
yaziditb@gmail.com <yaziditb@gmail.com>
2018-05-11 18:18:01 +0700

    Add existing file

--- biomedy.github.io/biomedy.github.io/upload.sh
+++ biomedy.github.io/biomedy.github.io/upload.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
-SOURCE=./_site/
+SOURCE=_site/
+UPLOAD=biomedy.github.io
 CWD=`pwd`
 
-cd $SOURCE
-git init
-git add .
-#git clone https://biomedy:1ndonesia@github.com/biomedy/biomedy.github.io.git
-git remote add origin https://biomedy:1ndonesia@github.com/biomedy/biomedy.github.io.git
-git clone origin master
-git pull https://biomedy:1ndonesia@github.com/biomedy/biomedy.github.io.git master
+cd $CWD
+
+#copy all
+cp -rf $CWD/$SOURCE/* $CWD/$UPLOAD
+cd $CWD/$UPLOAD
+#git init
+git clone https://biomedy:1ndonesia@github.com/biomedy/biomedy.github.io.git
 git config user.email "yaziditb@gmail.com"
 git config user.name "yaziditb@gmail.com"
 
 git add --all .
 git commit -m "Add existing file"
-git config user.email "yaziditb@gmail.com"
-git config user.name "yaziditb@gmail.com"
-git push  origin  master
-
+#git config user.email "yaziditb@gmail.com"
+#git config user.name "yaziditb@gmail.com"
+git push origin master
